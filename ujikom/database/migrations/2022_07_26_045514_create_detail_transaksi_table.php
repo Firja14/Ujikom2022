a15cc7e304e07ddf36d99604225735dbe4b7d00f
Firja14 <mrf140905@gmail.com>
2022-07-26 13:19:51 +0700

    UJIKOM2022

---
+++ ujikom/database/migrations/2022_07_26_045514_create_detail_transaksi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('produk_id')->constrained();
+            $table->foreignId('transaksi_id')->nullable()->constrained();
+            $table->integer('qty');
+            $table->enum('status',['keranjang','cekout']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_transaksi');
+    }
+}
---
+++ ujikom/resources/views/template/nav.blade.php
@@ -0,0 +1,14 @@
+<ul class="nav nav-pills">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="https://m.facebook.com/login/?locale=id_ID&refsrc=deprecated">Facebook</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="https://www.instagram.com/accounts/login/">Instagram</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="https://access.line.me/oauth2/v2.1/login?loginState=4jZB6HnD2lFK9oGZ2PL1r5&loginChannelId=1397264795&returnUri=%2Foauth2%2Fv2.1%2Fauthorize%2Fconsent%3Fscope%3Dprofile%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fcreator.line.me%252Fsignup%252Fline_callback%26state%3Dd3cxprdfsEX6N3ClzLG00Bv0PRkVqMhM%26client_id%3D1397264795#/">Line</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Whatsapp</a>
+    </li>
+  </ul>
